5418b71761d8de5ea76b04abcaa1021d29eacf2d
gregorsunta <gresunta@gmail.com>
2022-12-13 20:46:01 +0100

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-  scripts: {
-    build: 'webpack --config webpack.config.js',
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Battleship',
+      template: './src/index.html',
+    }),
+  ],
 };

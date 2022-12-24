70a851eef53e7e219e31db1659187784fc467168
gregorsunta <gresunta@gmail.com>
2022-12-24 17:18:45 +0100

    add scss loaders to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,17 @@ export default {
   module: {
     rules: [
       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
         test: /\.jsx?$/,
         exclude: ['/' + '/node_modules'],
         use: ['babel-loader'],
